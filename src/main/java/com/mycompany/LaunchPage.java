cfe529fe4b8a3d911438cc15aac476dcd23543f1
Shawn McKinney <shawn.mckinney@jts.us>
2013-09-08 17:18:21 -0500

    add redirect

--- src/main/java/com/mycompany/LaunchPage.java
+++ src/main/java/com/mycompany/LaunchPage.java
@@ -5,6 +5,7 @@ package com.mycompany;
 
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.http.handler.RedirectRequestHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
@@ -17,13 +18,12 @@ public class LaunchPage extends MyBasePage
 {
     public LaunchPage()
     {
- /*       HttpServletRequest servletReq = (HttpServletRequest)getRequest().getContainerRequest();
+        HttpServletRequest servletReq = (HttpServletRequest)getRequest().getContainerRequest();
         Principal principal = servletReq.getUserPrincipal();
         if(principal == null)
         {
-            setResponsePage(LoginPage.class);
+            getRequestCycle().scheduleRequestHandlerAfterCurrent(new RedirectRequestHandler("/login/login.html"));
         }
- */
         add(new Label("label1", "You have access to the link(s) above."));
     }
 }
